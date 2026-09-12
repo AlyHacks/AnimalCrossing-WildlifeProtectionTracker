bf64eba8f04ec4a6e7ce09b3e03ffb1501137ca5
AlyHacks <alyasu929@gmail.com>
2026-09-11 22:12:28 -0400

    distance warning

--- animal_detection/animaldetection.py
+++ animal_detection/animaldetection.py
@@ -142,9 +142,9 @@ def object_localization(correct_frame, animal_det):
 def audio(cx, animal, animal_det, distance): #FIX TO AUDIO
     x = cx/640   #the position of object is a fraction from 0 to 1, 0 is left#turns on the led for a time based on distance
     if (cx>0 and cx<320) and animal_det is True:
-        text = f"{animal} detected on the left side"
+        text = f"{animal} detected on the left side {distance} meters away"
     elif (cx>=320 and cx<640) and animal_det is True:
-        text = f"{animal} detected on the right side"
+        text = f"{animal} detected on the right side {distance} meters away"
     else:
         animal_det = False
         text = 'none'
